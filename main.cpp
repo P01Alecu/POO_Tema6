dcae35ae1239b5216a66cf394b276137530ee31b
Gabriel Alecu <36172431+P01Alecu@users.noreply.github.com>
2020-04-03 02:41:05 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -14,11 +14,12 @@ public:
     persoana(persoana&);
     ~persoana();
     void set(int, string, string);
-    void set(persoana&);
+    void set(const persoana&);
     virtual void citire(istream& in);
     friend istream& operator>>(istream& in, persoana& p);
     virtual void afisare(ostream& out);
     friend ostream& operator<<(ostream& out, persoana& p);
+    void operator=(const persoana&);
 
 };
 persoana::persoana(int id = 0, string nume = "", string cnp = ""){
@@ -43,7 +44,7 @@ void persoana::set(int id = 0, string nume = "", string cnp = ""){
     this->nume = nume;
     this->cnp = cnp;
 }
-void persoana::set(persoana& p){
+void persoana::set(const persoana& p){
     this->id = p.id;
     this->nume = p.nume;
     this->cnp = p.cnp;
@@ -70,6 +71,11 @@ ostream& operator<<(ostream& out, persoana& p){
     p.afisare(out);
     return out;
 }
+void persoana::operator=(const persoana& p){
+    this->id = p.id;
+    this->nume = p.nume;
+    this->cnp = p.cnp;
+}
 
 /////////////////////////////////////////////abonat
 class abonat:public persoana{
@@ -85,12 +91,14 @@ public:
     friend istream& operator>>(istream& in, abonat& p);
     void afisare(ostream& out);
     friend ostream& operator<<(ostream& out, abonat& p);
+    void operator=(const abonat&);
 };
 
 abonat::abonat(int id = 0, string nume = "", string cnp = "" ,string nr_telefon = ""):persoana(id, nume, cnp){
     this->nr_telefon = nr_telefon;
 }
 abonat::~abonat(){
+    nr_telefon = "";
 }
 abonat::abonat(abonat& p){
     persoana::set(p);
@@ -126,9 +134,14 @@ ostream& operator<<(ostream& out, abonat& p){
     p.afisare(out);
     return out;
 }
+void abonat::operator=(const abonat& p){
+    persoana::set(p);
+    this->nr_telefon = p.nr_telefon;
+}
 
 //////////////////////////////////////////////////////abonament
 class abonament{
+protected:
     string nume_abonament;
     float pret;
     int perioada;
@@ -138,12 +151,13 @@ public:
     ~abonament();
 
     void set(string, float, int);
-    void set(abonament&);
+    void set(const abonament&);
 
     void citire(istream& in);
     friend istream& operator>>(istream& in, abonament& p);
     void afisare(ostream& out);
     friend ostream& operator<<(ostream& out, abonament& p);
+    void operator=(const abonament&);
 };
 
 abonament::abonament(string nume_abonament = "", float pret = 0, int perioada = 0){
@@ -167,7 +181,7 @@ void abonament::set(string nume_abonament, float pret, int perioada){
     this->pret = pret;
     this->perioada = perioada;
 }
-void abonament::set(abonament& p){
+void abonament::set(const abonament& p){
     this->nume_abonament = p.nume_abonament;
     this->pret = p.pret;
     this->perioada = p.perioada;
@@ -186,15 +200,100 @@ istream& operator>>(istream& in, abonament& p){
     return in;
 }
 void abonament::afisare(ostream& out){
-    cout<< "Nume abonament: " << nume_abonament << "\tPret abonament: " << pret << "\tPerioada(luni): "<< perioada << "\n";
+    cout<< "Nume abonament: " << nume_abonament << "\tPret abonament: " << pret << "\tPerioada(luni): "<< perioada << "\t";
 }
 ostream& operator<<(ostream& out, abonament& p){
     p.afisare(out);
     return out;
 }
+void abonament::operator=(const abonament& p){
+    this->nume_abonament = p.nume_abonament;
+    this->pret = p.pret;
+    this->perioada = p.perioada;
+}
+
+//////////////////////////////////////////////////////////abonament_premium
+class abonament_premium:public abonament{
+    int reducere;
+public:
+    abonament_premium(string, float, int, int);
+    ~abonament_premium();
+
+    void set(string, float, int, int);
+    void set(const abonament_premium&);
+
+    void citire(istream& in);
+    void afisare(ostream& out);
+    friend istream& operator>>(istream& in, abonament_premium& p);
+    friend ostream& operator<<(ostream& out, abonament_premium& p);
+
+    void operator=(const abonament_premium& p);
+};
+
+abonament_premium::abonament_premium(string nume_abonament = "", float pret = 0, int perioada = 0, int reducere = 0):abonament(nume_abonament, pret, perioada){
+    this->reducere = reducere;
+}
+
+abonament_premium::~abonament_premium(){
+    reducere = 0;
+}
+void abonament_premium::set(string nume_abonament, float pret, int perioada, int reducere){
+    abonament::set(nume_abonament, pret, perioada);
+    this->reducere = reducere;
+}
+void abonament_premium::set(const abonament_premium& p){
+    abonament::set(p);
+    this->reducere = p.reducere;
+}
+
+void abonament_premium::citire(istream& in){
+    abonament::citire(in);
+    cout << "Reducere: " << "\n";
+    in >> reducere;
+}
+istream& operator>>(istream& in, abonament_premium& p){
+    p.citire(in);
+    return in;
+}
+void abonament_premium::afisare(ostream& out){
+    abonament::afisare(out);
+    out << "Reducere: " << reducere << "\n";
+}
+ostream& operator<<(ostream& out, abonament_premium& p){
+    p.afisare(out);
+    return out;
+}
+void abonament_premium::operator=(const abonament_premium& p){
+    abonament::set(p);
+    this->reducere = p.reducere;
+}
+
+/////////////////////////////////////////////////////////////////clienti
+class clienti{
+    abonat* a;
+    abonament* ab;
+    abonament_premium* abp;
+public:
+    clienti(abonat&);
+};
+
+
+
 
 int main()
 {
-    abonat ab;
+    persoana a;
+    persoana b;
+    a.set(1, "nume", "cnp");
+    b=a;
+    cout<<b<<endl;
+
+    abonat a1;
+    abonat b1;
+    a1.set(12, "nume", "cnp", "tel");
+    b1=a1;
+    cout<<b1;
+
+
     return 0;
 }
